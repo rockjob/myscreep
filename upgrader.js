7f5ffd8bc334e522dc35fca6098b811722ea5435
rockjob <jprice@magrit.int>
2017-03-22 14:54:02 -0400

    Improved upgrader

--- upgrader.js
+++ upgrader.js
@@ -24,17 +24,37 @@ module.exports = {
     }else{
       //var target = creep.room.find(FIND_SOURCES);
       //Look for containers
-      var target = creep.pos.findClosestByPath(_.filter(Game.spawns.Spawn1.room.find(FIND_STRUCTURES),function(x){return x.structureType== STRUCTURE_CONTAINER && x.store[RESOURCE_ENERGY] > 250} ));
-      if(target){
-        if(creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-          creep.moveTo(target);
+
+      if(creep.memory.target){
+        //check if still exists
+        //check if has enough resources
+        if(!Game.getObjectById(creep.memory.target){
+          updateTarget();
         }
-      }else {  if(Game.spawns.Spawn1.energy == Game.spawns.Spawn1.energyCapacity){
-        if(creep.withdraw(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-          creep.moveTo(Game.spawns.Spawn1);
+        if(Game.getObjectById(creep.memory.target).store[RESOURCE_ENERGY] < creep.energyCapacity){
+          updateTarget();
         }
+        if(creep.withdraw(Game.getObjectById(creep.memory.target), RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+          creep.moveTo(Game.getObjectById(creep.memory.target));
+        }
+      }else {
+        updateTarget();
+
+        `if(Game.spawns.Spawn1.energy == Game.spawns.Spawn1.energyCapacity){
+          if(creep.withdraw(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+            creep.moveTo(Game.spawns.Spawn1);
+          }
+          `}
+          else {
+            creep.moveTo(Game.flags.Flag1);
+          }
+        }
+
+
+
+
       }
-    }
-  }
-}
-};
+      function updateTarget(){
+        creep.memory.target = creep.pos.findClosestByPath(_.filter(Game.spawns.Spawn1.room.find(FIND_STRUCTURES),function(x){return x.structureType== STRUCTURE_CONTAINER && x.store[RESOURCE_ENERGY] >  creep.energyCapacity} ).id);
+      }
+    };
